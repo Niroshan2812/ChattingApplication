8e54af9c96872c852fafc42e0c8ac788a9a6f8e4
Niroshan Dharmasiri <84610063+Niroshan2812@users.noreply.github.com>
2022-10-24 14:50:56 +0530

    Add files via upload

--- App.js
+++ App.js
@@ -1,10 +1,15 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import ChartScreens from './src/screens/ChatScreens';
+
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+
+      <ChartScreens/>
+
+
       <StatusBar style="auto" />
     </View>
   );
@@ -14,7 +19,8 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
     justifyContent: 'center',
+    paddingVertical:50,
   },
 });
+
